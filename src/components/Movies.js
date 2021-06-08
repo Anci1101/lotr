ae2322626361b3a558da35f3e9e6595681fb4ea7
Anci1101 <51233955+Anci1101@users.noreply.github.com>
2021-06-08 17:38:13 +0200

    Revert "Development"

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,44 +1,20 @@
-import React, { useContext, useState} from 'react'
+import React, { useContext } from 'react'
 import { MovieContext } from '../contexts/MovieContext'
-import MovieDetails from './MovieDetails';
-import MovieList from './MovieList';
 
 const Movies = () => {
     const {movies, loading, error} = useContext(MovieContext)
     console.log(movies, 'movies');
-
-    const [selectedMovie, setSelectedMovie] = useState({})
-    const [showMovieDetails, setShowMovieDetails] = useState(false)
-
-    const handleMovieDetails = (id) => {
-        console.log('anabanaan');
-        const filteredMovie = movies.filter((item)=> id === item._id)
-        // console.log(filteredMovie[0]);
-        setSelectedMovie(filteredMovie[0])
-        setShowMovieDetails(true)
-    }
-
-
     return (
         <div>
             Movie component
             <h2>Movies</h2>
             {loading && <div>Loading...</div>}
             {error && <div>{error}</div>}
-            {movies && (movies.map((movie)=>(
-                <div key={movie._id}>
-                    <MovieList 
-                        movieId={movie._id}  
-                        name={movie.name}
-                        handleMovieDetails={handleMovieDetails}
-                    />
+            {movies && (movies.map((item)=>(
+                <div key={item._id}>
+                    <p>{item.name}</p>
                 </div>
             )))}
-            <div>
-                {showMovieDetails && (<div>
-                    <MovieDetails selectedMovie={selectedMovie}/>
-                    </div>)}
-            </div>
             
 
         </div>
